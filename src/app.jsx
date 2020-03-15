0dc4e88552d38b37c518f936a10dc94fb0d206bc
AnnaTokareva55 <edelveis2606@yandex.ru>
2020-03-15 21:19:30 +0300

    correction browserrouter

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import { BrowserRouter } from "react-router-dom";
 import { Layout } from "./components/Layout/Layout";
 import { InstallPopup } from "./components/InstallPopup/InstallPopup";
 
@@ -7,10 +8,10 @@ export class App extends Component {
   render() {
     return (
       <MuiThemeProvider>
-        <>
+        <BrowserRouter basename="/react-messenger-deploy">
           <Layout />
           <InstallPopup />
-        </>
+        </BrowserRouter>
       </MuiThemeProvider>
     );
   }
